69ffe1758dbb574a08d43956a947ddd13d332e8e
Tochka <tochka114@gmail.com>
2024-05-04 11:22:06 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,27 @@
 class StringBuilder{
-    initialValue='' 
+    #value;
+    constructor(initialValue){
+        this.#value=initialValue
+    }
+    getValue(){
+        return this.#value
+    } 
+    padEnd(str){
+      return  this.#value+=`${str}`
+    }
+    padStart(str){
+        return  this.#value=`${str}${this.#value}`
+    }
+    padBoth(str){
+        return  this.#value=`${str}${this.#value}${str}`
+    }
 }
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
