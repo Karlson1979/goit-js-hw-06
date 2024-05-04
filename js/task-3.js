8cf1f95f2eb9016eaa708f836517b1ed5d310017
Tochka <tochka114@gmail.com>
2024-05-04 15:29:44 +0300

    format

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,20 @@
-class StringBuilder{
-    #value;
-    constructor(initialValue){
-        this.#value=initialValue
-    }
-    getValue(){
-        return this.#value
-    } 
-    padEnd(str){
-      return  this.#value+=`${str}`
-    }
-    padStart(str){
-        return  this.#value=`${str}${this.#value}`
-    }
-    padBoth(str){
-        return  this.#value=`${str}${this.#value}${str}`
-    }
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    return (this.#value += `${str}`);
+  }
+  padStart(str) {
+    return (this.#value = `${str}${this.#value}`);
+  }
+  padBoth(str) {
+    return (this.#value = `${str}${this.#value}${str}`);
+  }
 }
 
 const builder = new StringBuilder(".");
